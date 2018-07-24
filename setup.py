a084ca78b81cc187519e9a07984dec6b213b3f60
irving <chenghuanl@genvis.co>
2018-07-24 16:15:03 +0800

    convert to package and connect

--- setup.py
+++ setup.py
@@ -11,5 +11,6 @@ setuptools.setup(
     version="0.1.0",
     url="https://github.com/irving-genvis/citest.git",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    test_suite='nose.collector',
+    tests_require=['nose'],
 )

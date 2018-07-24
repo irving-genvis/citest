075d8f75fc1da0a6b2c63abbde57811073b4d493
irving <chenghuanl@genvis.co>
2018-07-24 14:33:29 +0800

    add package

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@
 '''
 
 import setuptools
+
 setuptools.setup(
     name="ci",
     version="0.1.0",
-    url="https://github.com/username/animalsounds",
+    url="https://github.com/irving-genvis/citest.git",
     packages=setuptools.find_packages(),
     install_requires=[],
 )

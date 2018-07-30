2c9fef37f0333624278da1ca3e341b91ff55a1db
irving <chenghuanl@genvis.co>
2018-07-30 10:59:22 +0800

    test again.

---
+++ ci/tests/test_2.py
@@ -0,0 +1,21 @@
+'''
+
+  Created by irving on 24/07/18
+
+'''
+from unittest import TestCase
+from ci.calculator import calculator
+
+
+class TestClaculator(TestCase):
+
+    def setUp(self):
+        self.c = calculator()
+
+    def test_add(self):
+        self.assertEqual(self.c.add(1, 1), 2)
+
+    def test_subtract(self):
+        self.assertEqual(self.c.subtract(1, 1), 0)
+
+
